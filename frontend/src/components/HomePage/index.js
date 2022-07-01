c49ab144f109f1c75b07e61c8dbe97335e944522
Mineh <minehgharabegi@gmail.com>
2022-07-01 01:00:19 -0700

    Navigation bar styling done

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,6 +2,6 @@ import { NavLink } from "react-router-dom";
 
 export default function HomePage() {
     return (
-        <h1>Paws</h1>
+        <h1></h1>
     )
 }
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -33,15 +33,15 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button className="profile-icon-button" onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
+          <li className="username" >{user.username}</li>
+          <li className="email" >{user.email}</li>
           <li>
-            <button onClick={logout}>Log Out</button>
+            <button className="logout-button" onClick={logout}>Log Out</button>
           </li>
         </ul>
       )}
