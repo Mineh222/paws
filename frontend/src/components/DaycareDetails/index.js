5a6e1837c27cba08a10d06f3c069800c317ca239
Mineh <minehgharabegi@gmail.com>
2022-07-03 00:06:09 -0700

    daycare details page styling done

--- frontend/src/components/DaycareDetails/index.js
+++ frontend/src/components/DaycareDetails/index.js
@@ -28,28 +28,32 @@ export default function DaycareDetails(){
 
     if (!sessionUser) {
         return (
-            <div key={daycare.id} className='daycare-container'>
+            <main className='daycare-details-container'>
                 <h2 className="daycare-title">{daycare.name}</h2>
-                <img className="daycare-image" alt='daycare' src={daycare.image}></img>
-                <p className="daycare-description"> {daycare.description}</p>
-                <p className="daycare-address">{daycare.address}</p>
-                <p className='daycare-phoneNumber'>{daycare.phoneNumber}</p>
-                <p className='daycare-businessHouse'>{daycare.businessHours}</p>
-                <Reviews />
-            </div>
+                <div key={daycare.id} className='daycare-container'>
+                    <img className="daycare-image" alt='daycare' src={daycare.image}></img>
+                    <h3>About Us</h3>
+                        <p className="daycare-description">{daycare.description}</p>
+                    <p className="daycare-address">Address: {daycare.address}</p>
+                    <p className='daycare-phoneNumber'>Contact Us: {daycare.phoneNumber}</p>
+                    <p className='daycare-businessHouse'>Hours Of Operation: {daycare.businessHours}</p>
+                    <Reviews />
+                </div>
+            </main>
         )
     }
 
     return (
-        <>
-             <div key={daycare.id} className='daycare-container'>
-                 <h2 className="daycare-title">{daycare.name}</h2>
-                 <img className="daycare-image" alt='daycare' src={daycare.image}></img>
-                 <p className="daycare-description"> {daycare.description}</p>
-                 <p className="daycare-address">{daycare.address}</p>
-                 <p className='daycare-phoneNumber'>{daycare.phoneNumber}</p>
-                 <p className='daycare-businessHouse'>{daycare.businessHours}</p>
-             </div>
+        <main className='daycare-details-container'>
+            <h2 className="daycare-title">{daycare.name}</h2>
+            <div key={daycare.id} className='daycare-container'>
+                <img className="daycare-image" alt='daycare' src={daycare.image}></img>
+                <h3>About Us</h3>
+                    <p className="daycare-description">{daycare.description}</p>
+                <p className="daycare-address">Address: {daycare.address}</p>
+                <p className='daycare-phoneNumber'>Contact Us: {daycare.phoneNumber}</p>
+                <p className='daycare-businessHouse'>Hours Of Operation: {daycare.businessHours}</p>
+            </div>
             {sessionUser.id === daycare.ownerId && (
                 <>
                     <button className="delete-daycare-button" onClick={onDeleteDaycare}>
@@ -62,6 +66,6 @@ export default function DaycareDetails(){
                 <ReviewFormModal />
                 )}
             <Reviews />
-        </>
+        </main>
     )
 }
