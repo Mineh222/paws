9913d78fcca69c1ec25adb8461b98115625d68fa
Mineh Gharabegi <minehgharabegi@gmail.com>
2022-09-07 21:47:07 -0700

    Review and save buttons styling done

--- frontend/src/components/ReviewFormModal/index.js
+++ frontend/src/components/ReviewFormModal/index.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import ReviewForm from './ReviewForm';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
 
 function ReviewFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button className="leave-review-modal-button" onClick={() => setShowModal(true)}>Leave a review!</button>
+      <button className="leave-review-modal-button" onClick={() => setShowModal(true)}>
+        <div className="review-button">
+          <StarOutlineIcon />
+          <div>Write a review</div>
+        </div>
+      </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <ReviewForm trigger={showModal} setTrigger={setShowModal}/>
